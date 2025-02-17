d115c72069751c65d94a45cb29a28eec21fb1b6e
pratik1707 <pratik.malang@gmail.com>
2025-02-17 15:34:25 -0500

    Time: 110 ms (5.56%), Space: 211.7 MB (98.61%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.rb
@@ -0,0 +1,48 @@
+# @param {Integer} n
+# @return {String[]}
+def generate_parenthesis(n)
+    @total = 2*n
+
+    res = []
+
+    paranthesis_helper(n, 0, [], res)
+
+    return res 
+end
+
+def paranthesis_helper(n, i, slate, res)
+    #p slate 
+    if i == @total and valid_para(slate)
+        res << slate.dup.join
+        return  
+    elsif i > @total
+        return 
+    end 
+
+    #n.times do 
+        slate << '('
+        paranthesis_helper(n, i+1, slate, res)
+        slate.pop 
+    #end  
+
+    #n.times do 
+        slate << ')'
+        paranthesis_helper(n, i+1, slate, res)
+        slate.pop 
+    #end  
+
+end 
+
+def valid_para(arr)
+    stack = []
+
+    for char in arr do 
+        if char == '('
+            stack << char
+        else
+            return false if stack.size == 0 or stack.pop != '('
+        end 
+    end 
+
+    return stack.size == 0
+end 
