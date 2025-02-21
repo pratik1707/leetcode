e120400f2f5dcc8a5e4853a16957b1965fbd49d8
pratik1707 <pratik.malang@gmail.com>
2025-02-21 17:04:43 -0500

    Time: 7 ms (54.55%), Space: 211.5 MB (100%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.rb
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.rb
@@ -13,11 +13,8 @@ end
 def ip_address_helper(s, i, slate, res, n, dots)     
     return if dots > 4
 
-    if i == n and dots == 4    
-        #p slate
-        #if valid_solution?(slate)
-            res << slate.dup.join('.')             
-        #end 
+    if i == n and dots == 4          
+        res << slate.dup.join('.') 
         return
     end 
 
@@ -25,13 +22,9 @@ def ip_address_helper(s, i, slate, res, n, dots)
         next if i+j > s.length  
         next if s[i...i+j].length != 1 and s[i] == '0'  
         next if s[i...i+j].to_i > 255 
-        slate  << s[i...i+j]
-        
+
+        slate  << s[i...i+j]        
         ip_address_helper(s, i+j, slate, res, n, dots+1)
         slate.pop
     end 
-end 
-
-def valid_solution?(arr)
-    
 end 
