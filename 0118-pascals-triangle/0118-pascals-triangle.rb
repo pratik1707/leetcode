645b2dafc4bcbb5eac1f8208cf64e1170ea4b2ea
pratik1707 <pratik.malang@gmail.com>
2025-02-24 17:10:15 -0500

    Time: 0 ms (100%), Space: 211.7 MB (28.18%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.rb
@@ -0,0 +1,22 @@
+# @param {Integer} num_rows
+# @return {Integer[][]}
+def generate(num_rows)
+    table = Array.new(num_rows){Array.new(num_rows, nil)}
+    table[0][0] = 1
+
+    for row in (1...num_rows) do 
+        table[row][0] = 1
+        table[row][row] = 1
+    end 
+
+    for row in (2...num_rows) do 
+        prev_row = row-1
+        col = 1 
+        while(col < row) do 
+            table[row][col] = table[prev_row][col-1] + table[prev_row][col]
+            col += 1 
+        end 
+    end 
+    
+    return table.map{|a| a.compact}
+end
