794d07fd8f5ad8b0da94a661f036e96f1354c0a5
pratik1707 <pratik.malang@gmail.com>
2025-02-27 17:14:47 -0500

    Time: 1508 ms (100%), Space: 260.2 MB (100%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.rb
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.rb
@@ -3,28 +3,59 @@
 def len_longest_fib_subseq(arr)
     n = arr.length 
 
-    nums_set = arr.to_set 
+    mapping = {}
     result = 0 
-    #[1,2,3,4,5,6,7,8]
-    # we take each starting point and try to add next to it and see if the added value is present 
-    # if present we add the next and added value again and keep on counting 
-    
 
-    for start in (0...n) do 
-        for ne in (start+1...n) do 
-            prev = arr[ne]
-            curr = arr[start] + arr[ne]
-            count = 2
+    for i in (0...n) do 
+        mapping[arr[i]] = i 
+    end 
 
-            while(nums_set.include?(curr)) do 
-                prev, curr = curr, curr + prev 
-                count += 1
-                result = [result, count].max 
+    table = Array.new(n+1){Array.new(n+1, 0)}
+    table[0][1] = 2 
+
+    for curr in (2...n) do 
+        for prev in (0...curr) do 
+            diff = arr[curr] - arr[prev]
+
+            if mapping.has_key?(diff) and diff < arr[prev]                
+                index = mapping[diff]
+                table[prev][curr] = 1 + table[index][prev]
+                result = [result, table[prev][curr]].max 
+            else
+                table[prev][curr] = 2 
             end 
-
         end 
     end 
 
     return result 
+
+    # nums_set = arr.to_set 
+    # result = 0 
+    # #[1,2,3,4,5,6,7,8]
+    # # we take each starting point and try to add next to it and see if the added value is present 
+    # # if present we add the next and added value again and keep on counting 
+
+
+    # for start in (0...n) do 
+    #     for ne in (start+1...n) do 
+    #         prev = arr[ne]
+    #         curr = arr[start] + arr[ne]
+    #         count = 2
+
+    #         while(nums_set.include?(curr)) do 
+    #             prev, curr = curr, curr + prev 
+    #             count += 1
+    #             result = [result, count].max 
+    #         end 
+
+    #     end 
+    # end 
+
+    # return result 
+
+   
+
+    
+
 
 end
