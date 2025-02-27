48fd651eb1d30e737f4a412807fe51a1ff49f16c
pratik1707 <pratik.malang@gmail.com>
2025-02-27 15:57:25 -0500

    Time: 1544 ms (100%), Space: 211.9 MB (100%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.rb
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.rb
@@ -6,6 +6,9 @@ def len_longest_fib_subseq(arr)
     nums_set = arr.to_set 
     result = 0 
     #[1,2,3,4,5,6,7,8]
+    # we take each starting point and try to add next to it and see if the added value is present 
+    # if present we add the next and added value again and keep on counting 
+    
 
     for start in (0...n) do 
         for ne in (start+1...n) do 
