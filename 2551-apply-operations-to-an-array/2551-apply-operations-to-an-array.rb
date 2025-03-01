4e99acba51ce6ca4ea1d5728150202448d1a6c0c
pratik1707 <pratik.malang@gmail.com>
2025-03-01 14:19:52 -0500

    Time: 3 ms (33.33%), Space: 211.8 MB (33.33%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.rb
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.rb
@@ -2,7 +2,7 @@
 # @return {Integer[]}
 def apply_operations(nums)
     n = nums.length 
-    result = []
+    #result = []
 
     for i in (0...n-1) do 
         if nums[i] == nums[i+1]
@@ -11,16 +11,35 @@ def apply_operations(nums)
         end 
     end 
 
-    for num in nums do 
-        if num != 0
-            result << num 
+    # for num in nums do 
+    #     if num != 0
+    #         result << num 
+    #     end 
+    # end 
+
+    # while(nums.length != result.length) do 
+    #     result << 0
+    # end 
+
+    # return result 
+
+    # [1,0,2,0,0,1]
+
+    non_zero = 0 
+    i = 0 
+
+    while(i < n) do 
+        if nums[i] != 0
+            nums[non_zero] = nums[i]
+            non_zero += 1
         end 
+        i += 1
     end 
 
-    while(nums.length != result.length) do 
-        result << 0
+    while(non_zero < n) do 
+        nums[non_zero] = 0
+        non_zero += 1 
     end 
-
-    return result 
     
+    return nums 
 end
