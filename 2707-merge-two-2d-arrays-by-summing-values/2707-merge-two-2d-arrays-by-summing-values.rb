a6bd8610280bbf47be81e468fef202f5096c5eab
pratik1707 <pratik.malang@gmail.com>
2025-03-02 14:05:08 -0500

    Time: 0 ms (100%), Space: 211.7 MB (33.33%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.rb
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.rb
@@ -2,15 +2,48 @@
 # @param {Integer[][]} nums2
 # @return {Integer[][]}
 def merge_arrays(nums1, nums2)
-    result_hash = Hash.new(0)
+    result = []
+    # result_hash = Hash.new(0)
 
-    for i in (0...nums1.length) do 
-        result_hash[nums1[i][0]] += nums1[i][1]
+    # for i in (0...nums1.length) do 
+    #     result_hash[nums1[i][0]] += nums1[i][1]
+    # end 
+
+    # for i in (0...nums2.length) do 
+    #     result_hash[nums2[i][0]] += nums2[i][1]
+    # end 
+
+    # return result_hash.sort_by{|k, v| k} 
+
+    m = nums1.length
+    n = nums2.length
+
+    i = 0 
+    j = 0 
+
+    while(i < m and j < n) do
+        if nums1[i][0] == nums2[j][0]
+            result << [nums1[i][0], (nums1[i][1] + nums2[j][1])]
+            i += 1
+            j += 1
+        elsif nums1[i][0] < nums2[j][0]
+            result << [nums1[i][0], (nums1[i][1] )]
+            i += 1 
+        else
+            result << [nums2[j][0], (nums2[j][1] )]
+            j += 1 
+        end 
     end 
 
-    for i in (0...nums2.length) do 
-        result_hash[nums2[i][0]] += nums2[i][1]
+    while(i < m) do 
+        result << [nums1[i][0], (nums1[i][1] )]
+        i += 1
     end 
 
-    return result_hash.sort_by{|k, v| k} 
+    while(j < n) do 
+        result << [nums2[j][0], (nums2[j][1] )]
+        j += 1 
+    end 
+
+    return result 
 end
