b22dbffaa15cc1e3dd26c3c8b9390cb77b967d98
pratik1707 <pratik.malang@gmail.com>
2025-03-12 09:29:18 -0400

    Time: 0 ms (100%), Space: 18.2 MB (22.96%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,13 +1,49 @@
 class Solution:
+    def get_lower_bound(self, nums):
+        left = 0 
+        right = len(nums) - 1
+        index = len(nums) 
+
+        while(left <= right):
+            mid = (left + right)//2 
+
+            if nums[mid] < 0:                
+                left = mid + 1
+            else:
+                index = mid
+                right = mid - 1         
+        
+        return index 
+
+    def get_upper_bound(self, nums):
+        left = 0 
+        right = len(nums) - 1
+        index = len(nums) 
+
+        while(left <= right):
+            mid = (left + right)//2 
+
+            if nums[mid] <= 0:                
+                left = mid + 1
+            else:
+                index = mid
+                right = mid - 1 
+        
+        return index 
+
     def maximumCount(self, nums: List[int]) -> int:
         pos_count = 0 
         neg_count = 0 
 
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                neg_count += 1
-            elif nums[i] > 0:
-                pos_count += 1
+        n = len(nums)
 
-        return max(pos_count, neg_count )
+        index_lower_bound = 0 
+
+        index_upper_bound = 0 
+
+        index_lower_bound = self.get_lower_bound(nums)
+
+        index_upper_bound = self.get_upper_bound(nums)
+        
+        return max(index_lower_bound , n - index_upper_bound)
         
