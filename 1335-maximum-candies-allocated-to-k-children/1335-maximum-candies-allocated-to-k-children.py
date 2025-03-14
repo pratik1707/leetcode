444b66289bdba18de299057787fce092443991c3
pratik1707 <pratik.malang@gmail.com>
2025-03-13 22:53:44 -0400

    Time: 482 ms (28.96%), Space: 29.8 MB (54%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -1,25 +1,31 @@
 class Solution:
-    def can_divide(self, count, candies):
+    def can_divide(self, count, candies, k):
         # 2 
         
         total_kids = 0 
         for i in range(len(candies)):
             total_kids += candies[i]//count             
         
-        return total_kids
+        if total_kids >= k:
+            return True 
+            
+        return False 
 
     def maximumCandies(self, candies: List[int], k: int) -> int:
         total = sum(candies)
         n = len(candies)
+
+        if total < k:
+            return 0 
 
         left = 1
         right = max(candies)    
         result = 0         
 
         while(left <= right):
-            mid = left + (right - left)//2          
+            mid = (left + right) //2             
 
-            if self.can_divide(mid, candies) >= k:      
+            if self.can_divide(mid, candies, k):      
                 result = mid                       
                 left = mid + 1
             else:                
