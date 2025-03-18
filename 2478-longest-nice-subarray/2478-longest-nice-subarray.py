dca4a244bee47109370e9af3be91625b8e846a9c
pratik1707 <pratik.malang@gmail.com>
2025-03-18 09:48:36 -0400

    Time: 22 ms (99.39%), Space: 31 MB (91.16%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -1,6 +1,10 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         result = 1 
+        # 011 & 100 = 0
+        # [1,3,8,48,10]
+        # 000001   000011   001000   110000  001010
+        # when 0 then do an or to the mask so that it will append the number bits
         
         n = len(nums)
         count = 1 
@@ -10,8 +14,7 @@ class Solution:
         while(i < n):
             j = i + 1
             mask = nums[i]
-            while(j < n and (mask & nums[j] == 0)):
-                #print(nums[i]&nums[j])                
+            while(j < n and (mask & nums[j] == 0)):                               
                 result = max(result, j - i +1)
                 mask |= nums[j]
                 j += 1 
