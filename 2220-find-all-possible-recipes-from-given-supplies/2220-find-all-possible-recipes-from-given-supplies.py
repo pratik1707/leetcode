13359bb7bea984bb6ef68f2cd55cafefd78a3a2b
pratik1707 <pratik.malang@gmail.com>
2025-03-21 17:37:02 -0400

    Time: 39 ms (84.76%), Space: 19.7 MB (58.27%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -1,37 +1,77 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+
         n = len(recipes)
         available = set(supplies)
-        recipe_queue = deque()
+        in_degree = [0]*n 
+        graph = defaultdict(list)
+        created_recipes = []
+        recipe_index = {}
 
         for i in range(n):
-            recipe_queue.append(i)
+            recipe_index[recipes[i]] = i 
 
-        created_recipes = []
+        for i in range(len(ingredients)):
+            ingr_list = ingredients[i]
+            for ingredient in ingr_list:
+                if ingredient not in available:
+                    in_degree[i] += 1 # i => recipe id basically 
+                    graph[ingredient].append(recipes[i])
 
-        last_available = -1
+        queue = deque()
+        print(in_degree)
+        print(graph)
 
-        while(len(available) > last_available):
-            last_available = len(available)
-            queue_size = len(recipe_queue)
+        for i in range(n):
+            if in_degree[i] == 0:
+                queue.append(i) 
 
-            while(queue_size > 0):
-                queue_size -= 1 
-                r = recipe_queue.popleft()
+        while(queue):
+            recipeidx = queue.popleft()
+            recipe = recipes[recipeidx]
+            created_recipes.append(recipe)
 
-                ingredient = ingredients[r]
-                val = True  
-                for ingr in ingredient:
-                    if ingr not in available:
-                        val = False 
+            other_recipes = graph[recipes[recipeidx]]
 
-                if val:
-                    created_recipes.append(recipes[r])
-                    available.add(recipes[r])
-                else:
-                    recipe_queue.append(r)
+            for other_rec in other_recipes:
+                r_idx = recipe_index[other_rec]
+                in_degree[r_idx] -= 1
+                if in_degree[r_idx] == 0:
+                    queue.append(r_idx)
 
         return created_recipes
+
+
+
+
+        # for i in range(n):
+        #     recipe_queue.append(i)
+
+        # created_recipes = []
+
+        # last_available = -1
+
+        # while(len(available) > last_available):
+        #     last_available = len(available)
+        #     queue_size = len(recipe_queue)
+
+        #     while(queue_size > 0):
+        #         queue_size -= 1 
+        #         r = recipe_queue.popleft()
+
+        #         ingredient = ingredients[r]
+        #         val = True  
+        #         for ingr in ingredient:
+        #             if ingr not in available:
+        #                 val = False 
+
+        #         if val:
+        #             created_recipes.append(recipes[r])
+        #             available.add(recipes[r])
+        #         else:
+        #             recipe_queue.append(r)
+
+        # return created_recipes
 
 
                 
