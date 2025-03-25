f0d635b7bd11cf1f8f1206b6e4d1d2f18683b4f6
pratik1707 <pratik.malang@gmail.com>
2025-03-25 11:53:57 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,45 @@
 class Solution:
+    def partition(self, freq, arr, start, end):
+        pivot = random.randint(start, end)
+
+        arr[start], arr[pivot] = arr[pivot], arr[start ]
+        smaller = start 
+
+        for bigger in range(smaller+1, end):
+            if freq[arr[bigger]] <= freq[arr[smaller]]:
+                smaller += 1
+                arr[smaller], arr[bigger] = arr[bigger], arr[smaller]
+        
+        arr[start], arr[smaller] = arr[smaller], arr[start]
+
+        return smaller 
+
+    def quick_select(self, freq, arr, k, n):
+        
+        start = 0 
+        end = n - 1 
+
+        while(start <= end):
+            pivot = self.partition(freq, arr, start, end)
+            
+            if pivot == n-k:
+                return 
+            elif pivot < n-k:
+                start = pivot + 1
+            else:
+                end = pivot - 1 
+
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
-        n = len(nums)
-        result = []
 
-        freq = [[-1*v, k] for k, v in freq.items()]
+        #print(freq.keys())
         
-        heapq.heapify(freq)
+        arr = list(freq.keys())
 
-        while(k > 0):
-            result.append(heapq.heappop(freq)[1])
-            k -= 1
-
-        return result 
-
+        n = len(arr)
         
+        self.quick_select(freq, arr, k, n)
 
-        
+        return arr[n-k:n]
         
