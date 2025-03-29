0f5467c2ec66acffea114cb45bdb03c78040086c
pratik1707 <pratik.malang@gmail.com>
2025-03-29 09:14:50 -0400

    Time: 10 ms (20.92%), Space: 212.3 MB (19.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rb
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rb
@@ -3,5 +3,21 @@
 def reverse_words(s)
     s.strip! 
 
-    return s.split.reverse.join(' ') 
+    n = s.length 
+
+    s = s.split
+
+    p s  
+
+    i = 0 
+    j = n-1 
+
+    while(i < j) do 
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1 
+
+    end 
+
+    return s.join(' ').strip  
 end
