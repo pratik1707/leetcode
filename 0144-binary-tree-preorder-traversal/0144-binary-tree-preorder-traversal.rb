c4640b20394a8bfe36937afb122bf6ff1c9740de
pratik1707 <pratik.malang@gmail.com>
2025-04-03 10:39:10 -0400

    Time: 0 ms (100%), Space: 211.3 MB (76.09%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.rb
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.rb
@@ -10,24 +10,50 @@
 # @param {TreeNode} root
 # @return {Integer[]}
 def preorder_traversal(root)
+    curr = root 
     res = []
 
-    preorder(root, res)
+    while(curr != nil) do 
+        if curr.left.nil?
+            res << curr.val 
+            curr = curr.right 
+        else
+            pre = curr.left 
+
+            while(pre.right != nil and pre.right != curr) do 
+                pre = pre.right 
+            end 
+
+            if pre.right.nil?
+                pre.right = curr 
+                res << curr.val 
+                curr = curr.left                 
+            else
+                pre.right = nil 
+                curr = curr.right
+            end 
+        end 
+    end 
 
     return res 
-end
+#     res = []
 
-def preorder(node, res)
-    return if node.nil? 
+#     preorder(root, res)
+
+#     return res 
+# end
+
+# def preorder(node, res)
+#     return if node.nil? 
     
-    res << node.val 
+#     res << node.val 
 
-    if !node.left.nil?
-        preorder(node.left, res)
-    end     
+#     if !node.left.nil?
+#         preorder(node.left, res)
+#     end     
 
-    if !node.right.nil? 
-        preorder(node.right, res)
-    end 
+#     if !node.right.nil? 
+#         preorder(node.right, res)
+#     end 
      
 end
