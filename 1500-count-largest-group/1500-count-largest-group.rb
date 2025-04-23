1ac21affb447ae23a235ff9cbdaaf6f7b28aff54
Pratik Malang <pratik.malang@gmail.com>
2025-04-23 14:14:35 -0400

    Time: 28 ms (50%), Space: 211.3 MB (100%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.rb
@@ -0,0 +1,33 @@
+# @param {Integer} n
+# @return {Integer}
+def count_largest_group(n)
+    count = 0 
+    map = Hash.new(0)
+
+    (1..n).each do |num|
+        sum = get_sum(num)
+        map[sum] += 1 
+    end 
+
+    max = map.values.max 
+
+    map = map.sort_by{|k, v| -v}.to_h 
+
+    p map 
+
+    for key, val in map do 
+        break if val < max
+        count += 1
+    end 
+
+    return count 
+end
+
+def get_sum(num)
+    s = 0 
+    while(num != 0) do 
+        s += num % 10
+        num = num/10
+    end 
+    return s 
+end 
